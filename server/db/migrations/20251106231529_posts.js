1726c3b42fa216999273d4278a9d733542547528
ravidassnz <ravidassnz@gmail.com>
2025-11-07 12:37:35 +1300

    added columns to migrantion posts

--- server/db/migrations/20251106231529_posts.js
+++ server/db/migrations/20251106231529_posts.js
@@ -1,15 +1,21 @@
 /**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
+ * @param {import('knex').Knex} knex
  */
-exports.up = function(knex) {
-  
-};
+export async function up(knex) {
+  return knex.schema.createTable('posts', (table) => {
+    table.increments('id').primary()
+    table.string('title', 255).notNullable()
+    table.text('content').notNullable()
+    table.string('mediaType', 20)
+    table.string('mediaURL', 255)
+    table.timestamps(true,true)
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+    // table.string('timestamp').defaultTo(knex.fn.now() isn't modern convention anymore, but it is Knex option. 
+
+
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('posts')
+}
