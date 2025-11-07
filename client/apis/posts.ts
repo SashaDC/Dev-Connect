0870ef91663c265f49055b1804733d3d1fbb20eb
ravidassnz <ravidassnz@gmail.com>
2025-11-07 14:59:19 +1300

    updated api on client side to add new post, get all posts, and to get by Id. will need to stick together once the userId has been created.

--- client/apis/posts.ts
+++ client/apis/posts.ts
@@ -1,10 +1,33 @@
 import request from 'superagent'
+import { Post, NewPost} from '../../models/posts'
 
 // fetch and/or create posts
 
-const rootURL = new URL(`/api/v1`, document.baseURI)
+const rootURL = new URL(`/api/v1/posts`, document.baseURI)
 
-export async function getFruits(): Promise<string[]> {
-  const response = await request.get(`${rootURL}/fruits`)
-  return response.body.fruits as string[]
+export async function getAllPosts(): Promise<string[]> {
+  const response = await request.get(`${rootURL}`)
+  return response.body.posts as Post[]
+}
+
+// post by id
+
+export async function getPostById(id: number): Promise<Post> {
+  const response = await request.get(`${rootURL}/${id}`)
+  return response.body as Post
+}
+
+// add new post - CRITICAL need to confirm this all works!!!
+
+export async function createPost(newPostData: NewPost, token: string): Promise<number> {
+  const response = await request
+    .post(rootURL)
+ 
+    .set('Authorization', `Bearer ${token}`)     // CRITICAL: Attach the JWT token to the request header
+    
+    .set('Content-Type', 'application/json')
+
+    .send(newPostData)
+    
+  return response.status 
 }
