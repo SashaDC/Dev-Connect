bbe423172ce07b746badf1ea0f88e00407f13327
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-09 11:28:38 +1300

    Basic starting routes and db stuff

--- server/db/users.ts
+++ server/db/users.ts
@@ -1,17 +1,33 @@
 import db from './connection.ts'
-import { Fruit, FruitData } from '../../models/fruit.ts'
 
-export async function getAllFruits() {
-  const fruit = await db('fruit').select()
-  return fruit as Fruit[]
+export async function getAllUsers() {
+  const users = await db('users').select()
+  return users
 }
 
-export async function getFruitById(id: number | string) {
-  const fruit = await db('fruit').select().first().where({ id })
-  return fruit as Fruit
+export async function getUserById(id: number | string) {
+  const user = await db('users').select().first().where({ id })
+  return user
 }
 
-export async function addFruit(data: FruitData) {
-  const [id] = await db('fruit').insert(data)
+export async function addUser(data) {
+  const [id] = await db('users').insert(data)
   return id
+}
+
+export async function updateUser(id, user) {} // Your code ravi
+
+export async function deleteUserById(id) {
+  return db('users').where({ id }).delete()
+}
+
+export async function userCanEdit(id, auth0Id) {
+  return db('users')
+    .where({ id })
+    .first()
+    .then((user) => {
+      if (user.added_by_user !== auth0Id) {
+        throw new Error('Unauthorized')
+      }
+    })
 }
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -2,45 +2,82 @@ import { Router } from 'express'
 import checkJwt, { JwtRequest } from '../auth0.ts'
 import { StatusCodes } from 'http-status-codes'
 
-import * as db from '../db/fruits.ts'
+import * as db from '../db/users.ts'
 
 const router = Router()
 
+// GET all users = Done
 router.get('/', async (req, res) => {
   try {
-    const fruits = await db.getAllFruits()
-
-    res.json({ fruits: fruits.map((fruit) => fruit.name) })
+    const users = await db.getAllUsers()
+    res.json({ users })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
   }
 })
 
+// GET specific user = Done
 router.get('/:id', async (req, res, next) => {
   try {
-    const fruit = await db.getFruitById(req.params.id)
-    res.json(fruit)
+    const user = await db.getUserById(req.params.id)
+    res.json(user)
   } catch (err) {
     next(err)
   }
 })
 
-router.post('/', checkJwt, async (req: JwtRequest, res, next) => {
+// POST(add) user =
+
+// PUT(update) users =
+router.put('/', checkJwt, async (req: JwtRequest, res) => {
+  const { user } = req.body
+  const id = Number(req.params.id)
+
+  if (!user || !id) {
+    console.error('Bad Request - no user or id')
+    return res.status(400).send('Bad request')
+  }
+
+  if (!req.auth?.sub) {
+    return res.sendStatus(StatusCodes.UNAUTHORIZED)
+  }
+
+  try {
+    await db.userCanEdit(id, req.auth?.sub)
+    const updatedUser = await db.updateUser(id, user)
+    res.status(200).json({ user: updatedUser })
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err)
+      if (err instanceof Error && err.message === 'Unauthorized') {
+        return res
+          .status(403)
+          .send('Unauthorized: Only the user who made the user may update it')
+      }
+      res.status(500).send('Something went wrong')
+    }
+  }
+})
+
+// DELETE user =
+router.delete('/:id', checkJwt, async (req: JwtRequest, res, next) => {
+  const id = Number(req.params.id)
+  if (!id) {
+    console.error('Invalid user id')
+    return res.status(400).send('Bad request')
+  }
   if (!req.auth?.sub) {
     res.sendStatus(StatusCodes.UNAUTHORIZED)
     return
   }
 
   try {
-    const { owner, name } = req.body
-    const id = await db.addFruit({ owner, name })
-    res
-      .setHeader('Location', `${req.baseUrl}/${id}`)
-      .sendStatus(StatusCodes.CREATED)
+    await db.userCanEdit(id, req.auth?.sub)
+    await db.deleteUserById(id)
+    res.sendStatus(StatusCodes.GONE)
   } catch (err) {
     next(err)
   }
 })
-
 export default router
